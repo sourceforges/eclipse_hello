a3cccc7209712f715373646d1e6e31611036da8c
sourceforges <39147598+sourceforges@users.noreply.github.com>
2018-05-15 12:41:21 +0900

    GitHub for git pull by JongChae

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
 
   public static void main(String[] args)
   {
+    // git commit --amend
     System.out.println("Hello World");
     System.out.println("World's End Dancehall");
     System.out.println("Earth's End Dancehall");
